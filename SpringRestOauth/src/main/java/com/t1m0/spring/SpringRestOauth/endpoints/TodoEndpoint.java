da8ce56e0dd3e7675fd4c810be144908c103a882
t1m0 <timo.schoepflin@gmail.com>
2014-05-31 21:11:03 +0200

    Update user stored in HSQL in Oauth project

--- SpringRestOauth/src/main/java/com/t1m0/spring/SpringRestOauth/endpoints/TodoEndpoint.java
+++ SpringRestOauth/src/main/java/com/t1m0/spring/SpringRestOauth/endpoints/TodoEndpoint.java
@@ -1,32 +1,33 @@
 /*
-* The MIT License (MIT)
-*
-* Copyright (c) 2014 Timo Schoepflin
-*
-* Permission is hereby granted, free of charge, to any person obtaining a copy
-* of this software and associated documentation files (the "Software"), to deal
-* in the Software without restriction, including without limitation the rights
-* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-* copies of the Software, and to permit persons to whom the Software is
-* furnished to do so, subject to the following conditions:
-*
-* The above copyright notice and this permission notice shall be included in all
-* copies or substantial portions of the Software.
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014 Timo Schoepflin
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
 
-* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-* SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package com.t1m0.spring.SpringRestOauth.endpoints;
 
 import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -38,6 +39,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.t1m0.spring.SpringRestOauth.entities.Todo;
+import com.t1m0.spring.SpringRestOauth.services.interfaces.LITodo;
 
 /**
  * The rest end point.
@@ -45,18 +47,21 @@ import com.t1m0.spring.SpringRestOauth.entities.Todo;
 @RestController
 @RequestMapping(value="/todo", produces=MediaType.APPLICATION_JSON_VALUE)
 public class TodoEndpoint {
-	
-	/** The id_counter. */
-	private long id_counter = 0;
-	
-	/** The todos. */
-	@SuppressWarnings("serial")
-	private Map<Long,Todo> todos = new HashMap<Long,Todo>(){{
-		put(id_counter,new Todo(id_counter, "Test 0", "Test 0"));
-		put(++id_counter,new Todo(id_counter, "Test 1", "Test 1"));
-		put(++id_counter,new Todo(id_counter, "Test 2", "Test 2"));
-	}};
-	
+
+	/** The dao. */
+	@Autowired
+	private LITodo dao;
+
+	@PostConstruct
+	private void inital(){
+		Todo t0 = new Todo("Todo 0", "Bla bla bla bla");
+		dao.create(t0);
+		Todo t1 = new Todo("Todo 0", "Bla bla bla bla");
+		dao.create(t1);
+		Todo t2 = new Todo("Todo 0", "Bla bla bla bla");
+		dao.create(t2);
+	}
+
 	/**
 	 * Gets a list of all todos.
 	 *
@@ -65,9 +70,9 @@ public class TodoEndpoint {
 	@ResponseStatus(value=HttpStatus.OK)
 	@RequestMapping(method=RequestMethod.GET)
 	public ResponseEntity<Collection<Todo>> getListTodo() {
-		return new ResponseEntity<Collection<Todo>>(todos.values(),HttpStatus.OK);
+		return new ResponseEntity<Collection<Todo>>(dao.all(),HttpStatus.OK);
 	}
-	
+
 	/**
 	 * Gets the requested todo.
 	 *
@@ -77,32 +82,32 @@ public class TodoEndpoint {
 	 */
 	@RequestMapping(method = RequestMethod.GET, value = "/{uid}")
 	public ResponseEntity<Todo> getTodo(@PathVariable long uid) {
-		return new ResponseEntity<Todo>(todos.get(uid),HttpStatus.OK);
+		return new ResponseEntity<Todo>(dao.read(uid),HttpStatus.OK);
 	}
-	
-    /**
+
+	/**
 	 * Adds the given todo.
 	 *
 	 * @param t
 	 *            the t
 	 * @return the response entity< todo>
 	 */
-    @RequestMapping(method = RequestMethod.POST, consumes={MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
-    public ResponseEntity<Todo> addTodo(@RequestBody Todo t) {
-		t.setUID(++id_counter);
-		todos.put(id_counter, t);
-		return new ResponseEntity<Todo>(t,HttpStatus.CREATED);
-    }
-    
-    /**
+	@RequestMapping(method = RequestMethod.POST, consumes={MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	public ResponseEntity<Todo> addTodo(@RequestBody Todo t) {
+		return new ResponseEntity<Todo>(dao.create(t),HttpStatus.CREATED);
+	}
+
+	/**
 	 * Deletes the given todo.
 	 *
 	 * @param uid
 	 *            the uid
 	 * @return the response entity< todo>
 	 */
-    @RequestMapping(method = RequestMethod.DELETE, value = "/{uid}")
-    ResponseEntity<Todo> deleteTodo(@PathVariable long uid) {
-    	return new ResponseEntity<Todo>(todos.remove(uid), HttpStatus.NOT_FOUND);
-    }
+	@RequestMapping(method = RequestMethod.DELETE, value = "/{uid}")
+	ResponseEntity<Todo> deleteTodo(@PathVariable long uid) {
+		Todo t = dao.read(uid);
+		dao.delete(t);
+		return new ResponseEntity<Todo>(t, HttpStatus.NOT_FOUND);
+	}
 }
